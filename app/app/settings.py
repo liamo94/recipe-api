962c61207189d7fb68b70453197e6280758bc5e2
Liam O'Donnell <liam094@hotmail.com>
2023-10-24 22:36:11 +0100

    Make json default output

--- app/app/settings.py
+++ app/app/settings.py
@@ -138,6 +138,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+    "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
 }
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
